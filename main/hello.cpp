ca62cc0937edfc85d3dcadc728caa36aacea628f
rpbhoomika <104298180+rpbhoomika@users.noreply.github.com>
2025-03-11 23:58:39 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "This is Bhoomika\nfrom cse pes hi\n";
+  cout << "This is Bhoomika\nfrom cse pes \n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
