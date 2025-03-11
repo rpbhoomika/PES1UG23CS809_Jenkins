0a7133411bc1c98942623c829703f7cecf734a08
Bhoomika <rpbbagepalli@gmail.com>
2025-03-11 10:26:50 +0000

    first commit

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "This is Bhoomika";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
